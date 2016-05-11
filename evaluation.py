cef27342e4d1a3dc761f01aa39013c47e03d7189
RogerMonkey <gufengoyx@qq.com>
2016-05-11 22:38:58 +0800

    init

---
+++ evaluation.py
@@ -0,0 +1,8 @@
+# the predict and answer are all in same dimensions.
+# noted that day is bias number such as 1,2,3
+# wish you enjoy my bad code.
+#                                by Roger
+###############################################################################
+
+def eval( predict, answer):
+    pass
